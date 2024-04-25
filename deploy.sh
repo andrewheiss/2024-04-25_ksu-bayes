2845b94182a5c37f6bf809d62513011aec4f62f8
Andrew Heiss <andrew@andrewheiss.com>
2024-04-25 19:19:55 -0400

    Fix location

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 REMOTE_HOST="ath-cloud"
-REMOTE_DIR="~/sites/talks.andrewheiss.com/public_html/2024-03-21_be-curious-not-judgmental"
+REMOTE_DIR="~/sites/talks.andrewheiss.com/public_html/2024-04-25_ksu-bayes"
 REMOTE_DEST=$REMOTE_HOST:$REMOTE_DIR
 
 echo "Uploading new changes to remote server..."
 echo
-rsync -crvP --exclude '*_cache' --delete . $REMOTE_DEST
+rsync -crvP --exclude '*_cache' --delete _site/ $REMOTE_DEST
